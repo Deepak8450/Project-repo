a267296eb68af20e07eeadfb6303fa428f8b1b63
DeepakDeepak8450 <sendeepak2026@gmail.com>
2025-01-09 08:15:01 +0530

    First commit

--- src/Componants/About.js
+++ src/Componants/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <h1>About me</h1>
       <div className='profile-info'>    
           <div className='about-container'>
-        <img src={profile} id='profileImg'/>
+        <img src={profile} id='profileImg'/> 
       </div>
       <div className='intro'>
         <p>Hi, <h2>I'm Deepak Sen</h2>, a passionate and detail-oriented <h2>Frontend Developer</h2> with a knack for creating visually stunning, responsive, and user-friendly web interfaces. I love turning ideas into reality through clean an exceptional user experience.<br></br><br></br> With a strong foundation in HTML5, CSS3, JavaScript and React.js, I specialize in building modern, dynamic, and interactive web applications. I believe in continuous learning and styling updated with the latest technology.</p>
